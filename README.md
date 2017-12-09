e2531f1f26111dd0ed01a4938963204086886dab
Web Developland <webdevelopland@protonmail.com>
2017-12-09 15:21:32 +0300

    hello world

---
+++ README.md
@@ -0,0 +1,153 @@
+# AssociativeArray
+## v1.0.0 ( last update: 9 dec 2017 )
+Associative array. Typescript included.
+
+### Install
+npm
+```javascript
+npm install associativearray --save
+```
+
+Node.js
+```javascript
+const { AssociativeArray } = require('associativearray');
+```
+
+Typescript
+```javascript
+import { AssociativeArray } from 'associativearray';
+```
+
+Browser
+```javascript
+<script src='/node_modules/associativearray/dist/associativearray-min.js'></script>
+<script>
+  var arr = new dist.AssociativeArray();
+</script>
+```
+
+### How to use
+```javascript
+var arr = new AssociativeArray();
+var dogid = arr.push(  "dog"  );
+arr.add( "garfield",   "cat"  );
+arr.add( "ara",        "bird" );
+arr.push(              "fish" );
+console.log(arr.stringify());
+
+arr.remove(dogid);
+arr.remove("garfield");
+arr.change("ara", "parrot");
+console.log(arr.stringify());
+```
+### Looping
+```javascript
+var arr = new AssociativeArray();
+
+arr.add("string name 1", "string 1" );
+arr.add("string name 2", "string 2" );
+arr.push(                "string 3" );
+arr.push(                "string 4" );
+
+for(let i in arr.names) {
+  console.log(arr.names[i] + ", " + arr.values[i]);
+  if (arr.values[i] === "string 3") break;
+}
+```
+### forEach
+```javascript
+var arr = new AssociativeArray();
+
+arr.add("string name 1", "string 1" );
+arr.add("string name 2", "string 2" );
+arr.push(                "string 3" );
+arr.push(                "string 4" );
+
+arr.forEach(( name, value ) => {
+  console.log(name + ", " + value);
+  if (value === "string 3") return "break"; // Break loop
+});
+```
+### search()
+```javascript
+var arr = new AssociativeArray();
+
+arr.add("string name 1", "string 1" );
+arr.add("string name 2", "string 2" );
+arr.add("string name 3", "string 3" );
+
+var name = arr.search("string 2"); // name = "string name 2"
+```
+### import from array and object
+```javascript
+var array = [1,2,3,4,5,6];
+var arr1 = new AssociativeArray();
+arr1.importArray(array);
+
+var object = {
+  garfield: "cat",
+  pluto: "dog"
+};
+var arr2 = new AssociativeArray();
+arr2.importObject(object);
+
+console.log(arr1.stringify());
+console.log(arr2.stringify());
+```
+### copy() & share()
+```javascript
+var arr1 = new AssociativeArray();
+
+name1 = arr1.push("bob1");
+name2 = arr1.push("bob2");
+name3 = arr1.push("bob3");
+name4 = arr1.push("oleg");
+name5 = arr1.push("mike");
+
+var arr2 = new AssociativeArray();
+arr1.share(arr2, [ name2, name3 ]); // arr1 -> arr2
+console.log(arr2.stringify());
+
+var arr3 = new AssociativeArray();
+arr3.copy(arr1); // arr3 <- arr1
+console.log(arr3.stringify());
+```
+### Two way binding
+```javascript
+var arr = new AssociativeArray();
+arr.add("man1", "Mike");
+arr.add("man2", "Sam");
+
+arr.values[ arr.keys["man2"] ] = "Bob";
+// But you can't do like that: arr.value("man2") = "Bob";
+```
+### How to output AssociativeArray
+```javascript
+console.log( arr.toString() ); // Without recursion
+console.log( arr.stringify() ); // With recursion
+```
+### Sort
+```javascript
+var arr = new AssociativeArray();
+for(let i=0; i<20; i++) {
+  arr.add( "name"+i, Math.round( Math.random() * 200 ));
+}
+
+console.log(arr.stringify());
+arr.sort((a, b) => {
+  // You are able to use a.name & a.value
+  if (a.value < b.value) return -1;
+  else if (a.value > b.value) return 1;
+  else return 0;
+});
+console.log(arr.stringify());
+```
+### Other methods
+```javascript
+arr.rename(name1, name2); // Rename element
+arr.reverse(); // Reverse AssociativeArray
+arr.shuffle(); // Shuffle AssociativeArray
+arr.object(); // Return object form
+arr.array(); // Return Array form
+arr.concat(arr1, arr2, arr3, ...); // Concat AssociativeArrays
+```
---
+++ src/associativearray.ts
@@ -0,0 +1,223 @@
+import { is, not, isObject, isArray, isString } from 'existjs';
+import { randstr } from './randstr';
+
+// Dictinary
+export class AssociativeArray {
+  public names: any[];
+  public values: any[];
+  public length: number = 0;
+  public keys: any;
+
+  private id: number;
+
+  constructor() {
+    this.names = [];
+    this.values = [];
+    this.id = 0;
+    this.keys = {};
+  }
+
+  // Add new element with name
+  add(name: string, value: any) {
+    if (is(this.keys[name])) {
+      this.change(name, value);
+      return;
+    }
+    this.names.push(name);
+    this.values.push(value);
+    this.length = this.names.length;
+    this.keys[name] = this.length - 1;
+  }
+
+  // Get value by name
+  value(name: string) {
+    return this.values[this.keys[name]];
+  }
+
+  // Add new unnamed element
+  push(value: any) {
+    var id: string = "unicid" + randstr(20) + this.id++;
+    this.add(id, value);
+    return id;
+  }
+
+  // Change value by name
+  change(name: string, value: any) {
+    if (not(this.keys[name])) {
+      return;
+    }
+    this.values[this.keys[name]] = value;
+  }
+
+  // Remote element by name
+  remove(name: string) {
+    if (not(this.keys[name])) {
+      return;
+    }
+    this.names.splice(this.keys[name], 1);
+    this.values.splice(this.keys[name], 1);
+    this.length = this.names.length;
+
+    delete this.keys[name];
+  }
+
+  // Get name by value. Only first occurrence. 
+  search(value: any) {
+    const i = this.values.indexOf(value);
+    if (i === -1) {
+      return undefined;
+    }
+    return this.names[i];
+  }
+
+  // Functional for
+  forEach(callback: Function) {
+    for (let i in this.names) {
+      let breakPoint = callback(this.names[i], this.values[i]);
+      if (breakPoint === "break") {
+        break;
+      }
+    }
+  }
+
+  // Copy AssociativeArray
+  copy(associativeArray: AssociativeArray) {
+    this.values = associativeArray.values.slice();
+    this.names = associativeArray.names.slice();
+    this.length = associativeArray.length;
+    this.id = Math.max(this.id, associativeArray.id);
+    this.updateKeys();
+  }
+
+  // Share values
+  share(associativeArray: AssociativeArray, names: Array<any>) {
+    for (let name of names) {
+      associativeArray.add(name, this.values[this.keys[name]]);
+    }
+    associativeArray.id = Math.max(associativeArray.id, this.id);
+  }
+
+  // Get string form of all elements
+  toString() {
+    var trace: string = "";
+    for (let i in this.names) {
+      trace += `[${this.names[i]}] ${this.values[i]}\n`;
+    };
+    return trace;
+  }
+
+  // Recursive output
+  stringify(tab: string = "") {
+    var trace: string = "";
+    for (let i in this.names) {
+      trace += `${tab}[${this.names[i]}] `;
+      if (isObject(this.values[i])) {
+        var classname = this.values[i].constructor.name;
+        if (classname === "AssociativeArray") {
+          trace += "\n" + this.values[i].stringify(tab + "__ ");
+        } else {
+          trace += JSON.stringify(this.values[i]) + "\n";
+        }
+      } else if (isArray(this.values[i])) {
+        trace += JSON.stringify(this.values[i]) + "\n";
+      } else if (isString(this.values[i])) {
+        trace += `"${this.values[i]}"\n`;
+      } else {
+        trace += this.values[i] + "\n";
+      }
+    };
+    return trace;
+  }
+
+  // Change name of element
+  rename(name: string, newname: string) {
+    this.add(newname, this.value(name));
+    this.remove(name);
+  }
+
+  // Reverse AssociativeArray
+  reverse() {
+    this.values.reverse();
+    this.names.reverse();
+  }
+
+  // Get object form
+  object() {
+    var object: any = {};
+    for (let i in this.names) {
+      object[this.names[i]] = this.values[i];
+    }
+    return object;
+  }
+
+  // Import from object form
+  importObject(object: any) {
+    for (let name in object) {
+      this.add(name, object[name]);
+    }
+  }
+
+  // Get complex array form
+  array() {
+    var complex = [];
+    for (let i in this.names) {
+      complex.push({
+        name: this.names[i],
+        value: this.values[i]
+      });
+    }
+    return complex;
+  }
+
+  // Import from array form
+  importArray(array: Array<any>) {
+    for (let v of array) {
+      this.push(v);
+    }
+  }
+
+  private updateKeys() {
+    var keys: any = {};
+    for (let i in this.names) {
+      keys[this.names[i]] = i;
+    }
+    this.keys = keys;
+  }
+
+  // Sort AssociativeArray
+  sort(handler: any) {
+    var array = this.array();
+    array.sort(handler);
+    for (let i in array) {
+      this.names[i] = array[i].name;
+      this.values[i] = array[i].value;
+    }
+    this.updateKeys();
+  }
+
+  // Shuffle AssociativeArray
+  shuffle() {
+    var array = this.array();
+    for (let i = array.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    }
+    for (let i in array) {
+      this.names[i] = array[i].name;
+      this.values[i] = array[i].value;
+    }
+    this.updateKeys();
+  }
+
+  // Concat AssociativeArrays
+  concat(...associativeArrays: AssociativeArray[]) {
+    for (let associativeArray of associativeArrays) {
+      this.names = this.names.concat(associativeArray.names);
+      this.values = this.values.concat(associativeArray.values);
+      this.id = Math.max(this.id, associativeArray.id);
+    }
+    this.length = this.names.length;
+    this.updateKeys();
+  }
+
+}
